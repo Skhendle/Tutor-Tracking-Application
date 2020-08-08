8f9a697e702bd993b39e4b0ba82f3f535d04e29a
Thabo-Makola <736277@students.wits.ac.za>
2020-08-08 04:32:22 +0200

    Trying to deploy to heroku

--- app/config.py
+++ app/config.py
@@ -9,3 +9,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
