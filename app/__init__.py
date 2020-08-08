fc728ac38c4bab801464850e1dbdca4703a80f79
Thabo-Makola <736277@students.wits.ac.za>
2020-08-08 23:53:09 +0200

    clearing files

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,6 @@ from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,23 +16,5 @@ db = SQLAlchemy(app)
 
 #flask-migrate instanciation
 Migrate(app,db)
-
-if app.config['LOG_TO_STDOUT']:
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    app.logger.addHandler(stream_handler)
-else:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/tutortracker.log',
-                                        maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-app.logger.setLevel(logging.INFO)
-app.logger.info('Tutotracker startup')
 
 from app import routes , models
