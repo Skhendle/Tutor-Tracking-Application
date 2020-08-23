46a14b54140675e08bed66b5908bc330b989f4fc
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2020-08-23 08:08:59 -0400

    Update test_app.py
    Testing if the test file will run

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,19 +2,44 @@ import os
 import tempfile
 
 import pytest
+import app
+from app import app, tutortracker, db
 
-from app import tutortracker
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
 
 
 @pytest.fixture
 def client():
-    db_fd, tutortracker.app.config['DATABASE'] = tempfile.mkstemp()
-    tutortracker.app.config['TESTING'] = True
+	db_fd, tutortracker.app.config['DATABASE'] = tempfile.mkstemp()
+	tutortracker.app.config['TESTING'] = True
 
-    with tutortracker.app.test_client() as client:
-        with tutortracker.app.app_context():
-            app.init_db()
-        yield client
+	with tutortracker.app.test_client() as client:
+		with app.app_context():
+			app.make_shell_context()
+		yield client
 
-    os.close(db_fd)
-    os.unlink(tutortracker.app.config['DATABASE'])
+	os.close(db_fd)
+	os.unlink(tutortracker.app.config['DATABASE'])
+	login(client, '12345', 'password')
+
+
+def test_index_page(client):
+	"""Checking if the app opens the indeex page"""
+
+	rv = client.get('/')
+	assert b'Welcome to TUTOR TRACKER' in rv.data
+
+def test_loggin_page(client):
+	"""Checking if the app opens the indeex page"""
+
+	rv = client.get('/login')
+	assert b'New at TUTOR TRACKER?' in rv.data
+
+
