be58d26cc917d11fa2ec21902aa1da870f9bbfd3
tswaitswai <tswaitswai.m@gmail.com>
2020-09-01 01:22:45 +0200

    Added User class, fixed relative ul

--- app/student/forms.py
+++ app/student/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError , SelectField , RadioField
 from wtforms.validators import DataRequired, Email, EqualTo , Length
-from app.models import Student
+from app.models import Student, User
 
 from app.auth.forms import validate_employee_student_number
 
