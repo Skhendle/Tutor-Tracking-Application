bc918dbad962228fb6a464d4082490c4f6ce3a8d
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2020-09-15 03:46:42 +0200

    Update test_tutor.py

--- tests/test_tutor.py
+++ tests/test_tutor.py
@@ -37,12 +37,6 @@ def test_login_pass(flask_app_client):
     assert request.status_code == 200   
 
     
-def test_tutor_logout(flask_app_client):
-    client = flask_app_client
-    request = client.post('/logout' , follow_redirects=True)
-    assert request.status_code == 200
-
-    
 def test_login_fail(flask_app_client):
     client = flask_app_client
     request = client.post('/login' , data=dict(
